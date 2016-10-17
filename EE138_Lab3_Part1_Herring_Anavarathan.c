a3cc6336aa08d182824f5ef89a8e6789ca2d9e48
wazomike <wazomike@gmail.com>
2016-10-17 16:00:50 -0700

    New Code

---
+++ EE138_Lab3_Part1_Herring_Anavarathan.c
@@ -0,0 +1,112 @@
+////////////////////////////////////////////////////////////////////////////////////// 
+////	Lab 3 - PWM
+////		-SAMPLE CODE DOES NOT WORK-
+////			- set up appropriate registers
+////			- set up TC pointer 
+//////////////////////////////////////////////////////////////////////////////////////
+
+#include <asf.h>
+void Simple_Clk_Init(void);
+void enable_port(void);
+void enable_tc_clocks(void);
+void enable_tc(void);
+
+//setup a the correct TC pointer for the corresponding pins. (use table 5-1 as a reference)
+//there are multiple TC #'s, choose the one that ties to the specified port utilized
+
+int main (void)
+{
+	Simple_Clk_Init();
+
+	/* Enable the timer*/
+	enable_tc();
+	
+	while(1)
+	{
+	
+	}
+}
+
+/* Set correct PA pins as TC pins for PWM operation */
+void enable_port(void)
+{
+	//setup pins
+}
+
+/* Perform Clock configuration to source the TC 
+1) ENABLE THE APBC CLOCK FOR THE CORREECT MODULE
+2) WRITE THE PROPER GENERIC CLOCK SELETION ID*/
+void enable_tc_clocks(void)
+{
+	PM->APBCMASK.reg |= ______________;  	// PM_APBCMASK_______ is in the ___ position
+	
+	uint32_t temp=_____________;   		// ID for ________ is __________  (see table 14-2)
+	temp |= 0<<8;         			//  Selection Generic clock generator 0
+	GCLK->CLKCTRL.reg=temp;   		//  Setup in the CLKCTRL register
+	GCLK->CLKCTRL.reg |= 0x1u << 14;    	// enable it.
+}
+
+/* Configure the basic timer/counter to have a period of________ or a frequency of _________  */
+void enable_tc(void)
+{
+	enable_port();
+	enable_tc_clocks();
+	
+	/* Set up CTRLA */
+	/* 
+	
+		ex: pt->COUNT8.registername.reg = x;
+	
+	Set the 
+	1) counter mode
+	2) prescaler
+	3) set the PRESCSYNC bits to PRESC from table 27-4 in the datasheet
+	*/
+
+	/* Write a suitable value to fix duty cycle and period.*/
+
+	/*Enable TC*/
+
+}
+
+//Simple Clock Initialization
+void Simple_Clk_Init(void)
+{
+	/* Various bits in the INTFLAG register can be set to one at startup.
+	   This will ensure that these bits are cleared */
+	
+	SYSCTRL->INTFLAG.reg = SYSCTRL_INTFLAG_BOD33RDY | SYSCTRL_INTFLAG_BOD33DET |
+			SYSCTRL_INTFLAG_DFLLRDY;
+			
+	system_flash_set_waitstates(0);  		//Clock_flash wait state =0
+
+	SYSCTRL_OSC8M_Type temp = SYSCTRL->OSC8M;      	/* for OSC8M initialization  */
+
+	temp.bit.PRESC    = 0;    			// no divide, i.e., set clock=8Mhz  (see page 170)
+	temp.bit.ONDEMAND = 1;    			//  On-demand is true
+	temp.bit.RUNSTDBY = 0;    			//  Standby is false
+	
+	SYSCTRL->OSC8M = temp;
+
+	SYSCTRL->OSC8M.reg |= 0x1u << 1;  		// SYSCTRL_OSC8M_ENABLE bit = bit-1 (page 170)
+	
+	PM->CPUSEL.reg = (uint32_t)0;    		// CPU and BUS clocks Divide by 1  (see page 110)
+	PM->APBASEL.reg = (uint32_t)0;     		// APBA clock 0= Divide by 1  (see page 110)
+	PM->APBBSEL.reg = (uint32_t)0;     		// APBB clock 0= Divide by 1  (see page 110)
+	PM->APBCSEL.reg = (uint32_t)0;     		// APBB clock 0= Divide by 1  (see page 110)
+
+	PM->APBAMASK.reg |= 01u<<3;   			// Enable Generic clock controller clock (page 127)
+
+	/* Software reset Generic clock to ensure it is re-initialized correctly */
+
+	GCLK->CTRL.reg = 0x1u << 0;   			// Reset gen. clock (see page 94)
+	while (GCLK->CTRL.reg & 0x1u ) {  /* Wait for reset to complete */ }
+	
+	// Initialization and enable generic clock #0
+
+	*((uint8_t*)&GCLK->GENDIV.reg) = 0;  		// Select GCLK0 (page 104, Table 14-10)
+
+	GCLK->GENDIV.reg  = 0x0100;   		 	// Divide by 1 for GCLK #0 (page 104)
+
+	GCLK->GENCTRL.reg = 0x030600;  		 	// GCLK#0 enable, Source=6(OSC8M), IDC=1 (page 101)
+}
