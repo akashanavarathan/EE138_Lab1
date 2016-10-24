59befb3a0316daea2f073027d81809a7503a33ef
Anavarathan <akash.anavarathan@gmail.com>
2016-10-24 16:15:45 -0700

    FINAL

--- EE138_Lab2_Part2_Herring_Anavarathan.c
+++ EE138_Lab2_Part2_Herring_Anavarathan.c
@@ -1,90 +1,98 @@
-//////////////////////////////////////////////////////////////////////////////////////
-// Lab 2: Part 2: DAC
-// Scott Herring and Akash Anavarathan
-// Creates a Sine Wave with a Frequency of 1 kHz
+////////////////////////////////////////////////////////////////////////////////////// 
+////	Lab 3 - PWM
+////	Scott Herring and Akash Anavarathan
+////	Set up the TC to display a Sine Wave
 //////////////////////////////////////////////////////////////////////////////////////
 
 #include <asf.h>
 #include <math.h>
 #define PI 3.14159
-
 void Simple_Clk_Init(void);
-void configure_dac(void);
-void configure_dac_clock(void);
-Dac *portdac = DAC; 
+void enable_port(void);
+void enable_tc_clocks(void);
+void enable_tc(void);
+int index = 0;
+int cycles = 256;
+const unsigned char sine_table_percent[256] = {0x80,0x83,0x86,0x89,0x8c,0x8f,0x92,0x95,0x98,0x9c,0x9f,0xa2,0xa5,0xa8,0xab,0xae,
+	0xb0,0xb3,0xb6,0xb9,0xbc,0xbf,0xc1,0xc4,0xc7,0xc9,0xcc,0xce,0xd1,0xd3,0xd5,0xd8,
+	0xda,0xdc,0xde,0xe0,0xe2,0xe4,0xe6,0xe8,0xea,0xec,0xed,0xef,0xf0,0xf2,0xf3,0xf5,
+	0xf6,0xf7,0xf8,0xf9,0xfa,0xfb,0xfc,0xfc,0xfd,0xfe,0xfe,0xff,0xff,0xff,0xff,0xff,
+	0xff,0xff,0xff,0xff,0xff,0xff,0xfe,0xfe,0xfd,0xfc,0xfc,0xfb,0xfa,0xf9,0xf8,0xf7,
+	0xf6,0xf5,0xf3,0xf2,0xf0,0xef,0xed,0xec,0xea,0xe8,0xe6,0xe4,0xe2,0xe0,0xde,0xdc,
+	0xda,0xd8,0xd5,0xd3,0xd1,0xce,0xcc,0xc9,0xc7,0xc4,0xc1,0xbf,0xbc,0xb9,0xb6,0xb3,
+	0xb0,0xae,0xab,0xa8,0xa5,0xa2,0x9f,0x9c,0x98,0x95,0x92,0x8f,0x8c,0x89,0x86,0x83,
+	0x80,0x7c,0x79,0x76,0x73,0x70,0x6d,0x6a,0x67,0x63,0x60,0x5d,0x5a,0x57,0x54,0x51,
+	0x4f,0x4c,0x49,0x46,0x43,0x40,0x3e,0x3b,0x38,0x36,0x33,0x31,0x2e,0x2c,0x2a,0x27,
+	0x25,0x23,0x21,0x1f,0x1d,0x1b,0x19,0x17,0x15,0x13,0x12,0x10,0x0f,0x0d,0x0c,0x0a,
+	0x09,0x08,0x07,0x06,0x05,0x04,0x03,0x03,0x02,0x01,0x01,0x00,0x00,0x00,0x00,0x00,
+	0x00,0x00,0x00,0x00,0x00,0x00,0x01,0x01,0x02,0x03,0x03,0x04,0x05,0x06,0x07,0x08,
+	0x09,0x0a,0x0c,0x0d,0x0f,0x10,0x12,0x13,0x15,0x17,0x19,0x1b,0x1d,0x1f,0x21,0x23,
+	0x25,0x27,0x2a,0x2c,0x2e,0x31,0x33,0x36,0x38,0x3b,0x3e,0x40,0x43,0x46,0x49,0x4c,
+	0x4f,0x51,0x54,0x57,0x5a,0x5d,0x60,0x63,0x67,0x6a,0x6d,0x70,0x73,0x76,0x79,0x7c};
 
-int sinArr[292]; // Array to store the values for the Sine Wave
-int cycles = 292; // Number of Samples to be taken in order to recreate the Sine Wave
-int index = 0; // Counter used to increment the values stored into the DATA register
+//setup a the correct TC pointer for the corresponding pins. (use table 5-1 as a reference)
+//there are multiple TC #'s, choose the one that ties to the specified port utilized
 
 
-int main(void)
+int main (void)
 {
+	Tc * tc = TC2;
+	TcCount8 *tcpointer = &(tc->COUNT8);
 	Simple_Clk_Init();
-	configure_dac_clock();
-	configure_dac();
-	
-	for (int store = 0; store < cycles; store++)
-		{
-			sinArr[store] = (((sin(2* PI* store/cycles) + 1.1) * 1023) / 3.3) / 2; // Creates an array of values that will replicate the Sine Wave
-		}
-	
-	while (1) {
 
-		while (portdac->STATUS.reg & DAC_STATUS_SYNCBUSY) 
-		{
-			/* Wait until the synchronization is complete */
-		};
-		
-		portdac->DATA.reg = sinArr[index++]; // Increment the Array and store into the DATA register as soon as Synchronization is complete
-		if (index >= cycles)
-		{
-			index = 0; // Reset the Index to 0, if it's greater than the number of samples taken
-		}
-
+	/* Enable the timer*/
+	enable_tc();
+	
+	while(1)
+	{
+		tcpointer->CC[1].reg = sine_table_percent[index++];
+		if(index >= cycles)
+			index = 0;
 	}
-
 }
 
-void configure_dac_clock(void)
+/* Set correct PA pins as TC pins for PWM operation */
+void enable_port(void)
 {
-	PM->APBCMASK.reg |= (1u << 18);		// PM_APBCMASK DAC is in the 18 position
-	
-	uint32_t temp = 0x1A; 			// ID for GCLK_DAC is 0x1A
-	temp |= 0<<8; 					// Selection Generic clock generator 0
-	GCLK->CLKCTRL.reg = temp; 			// Setup in the CLKCTRL register
-	GCLK->CLKCTRL.reg |= 0x1u << 14; 		// enable it.
-}
-
-void configure_dac(void)
-{
-	//set pin as output for the dac
 	Port *ports = PORT_INSTS;
 	PortGroup *por = &(ports->Group[0]);
 	
-	por->PINCFG[2].bit.PMUXEN = 0x1;		// set to correct pin configuration
-	por->PMUX[1].bit.PMUXE = 0x1;			// set to correct peripheral
+	por->PINCFG[13].bit.PMUXEN = 0x1;		// set to correct pin configuration
+	por->PMUX[6].bit.PMUXO = 0x4;			// set to correct peripheral
+}
 
+/* Perform Clock configuration to source the TC 
+1) ENABLE THE APBC CLOCK FOR THE CORREECT MODULE
+2) WRITE THE PROPER GENERIC CLOCK SELETION ID*/
+void enable_tc_clocks(void)
+{
+	PM->APBCMASK.reg |= (1u << 10);  	// PM_APBCMASK_______ is in the ___ position
+	
+	uint32_t temp= 0x14;   		// ID for ________ is __________  (see table 14-2)
+	temp |= 0<<8;         			//  Selection Generic clock generator 0
+	GCLK->CLKCTRL.reg=temp;   		//  Setup in the CLKCTRL register
+	GCLK->CLKCTRL.reg |= 0x1u << 14;    	// enable it.
+}
 
-	while (portdac->STATUS.reg & DAC_STATUS_SYNCBUSY) {
-		/* Wait until the synchronization is complete */
-	}
+/* Configure the basic timer/counter to have a period of________ or a frequency of _________  */
+void enable_tc(void)
+{
+	Tc * tc = TC2;
+	TcCount8 *tcpointer = &(tc->COUNT8);
+	enable_port();
+	enable_tc_clocks();
+	
+	/* Set up CTRLA */
+	tcpointer->CTRLA.reg = (1u << 2); // set counter mode
+	tcpointer->CTRLA.reg |= (0x0 << 8); // prescaler set
+	tcpointer->CTRLA.reg |= (1u << 12); // PRESCSYNC set to PRESC
 
-	/* Set reference voltage with CTRLB */
-	portdac->CTRLB.reg = (1u << 6);
-
-
-	while (portdac->STATUS.reg & DAC_STATUS_SYNCBUSY) 
-	{
-		/* Wait until the synchronization is complete */
-	}
-
-	/* Enable the module with CTRLA */
-	portdac->CTRLA.reg = (1u << 1);
-
-	/* Enable selected output with CTRLB*/
-	portdac->CTRLB.reg = (1u << 0);
-
+	/* Write a suitable value to fix duty cycle and period.*/
+	tcpointer->CTRLA.reg |= (1u << 6); // NPWM Chosen in Wavegen
+	tcpointer->PER.reg = 0xFF;
+	/*Enable TC*/
+	tcpointer->CTRLA.reg |= (1u << 1); // Enable TC2
+	
 }
 
 //Simple Clock Initialization
@@ -96,7 +104,7 @@ void Simple_Clk_Init(void)
 	SYSCTRL->INTFLAG.reg = SYSCTRL_INTFLAG_BOD33RDY | SYSCTRL_INTFLAG_BOD33DET |
 			SYSCTRL_INTFLAG_DFLLRDY;
 			
-	system_flash_set_waitstates(0);  		//Clock_flash wait state = 0
+	system_flash_set_waitstates(0);  		//Clock_flash wait state =0
 
 	SYSCTRL_OSC8M_Type temp = SYSCTRL->OSC8M;      	/* for OSC8M initialization  */
 
